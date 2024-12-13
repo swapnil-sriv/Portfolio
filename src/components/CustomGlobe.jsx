5b8c97e0113a737a60b54786e15bfc1d6d456491
Swapnil <swapnilsrivastava2002@gmail.com>
2024-12-14 02:50:02 +0530

    Changed globe background

--- src/components/CustomGlobe.jsx
+++ src/components/CustomGlobe.jsx
@@ -34,7 +34,7 @@ const CustomGlobe = () => {
         showGraticules
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
         bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+        // backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
       />);
       }
    
--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -26,7 +26,7 @@ const Project = () => {
     <section className='c-space my-20' id='project'>
         <p className='head-text'>My work</p>
         <div className='grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full'>
-            <div className='flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200 backdrop-blur-md'>
+            <div className='flex flex-col gap-5 relative sm:p-10 py-10 px-5 justify-between shadow-2xl shadow-black-200 backdrop-blur-md'>
                 <div className='absolute top-0 right-0'>
                     <img src={currentProject.spotlight} alt="spotlight" className='w-full h-96 object-cover rounded-xl' />
                 </div>
